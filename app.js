fab5303d9d9f0b109e09a2a1eda651ad8c3e203a
akhterjawad <akhterjawad566@gmail.com>
2024-04-24 00:40:09 +0500

    complete

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-let h2 = document.querySelector('h2')
+let h2 = document.querySelector('.h2')
+let h22 = document.querySelector('.h22')
 
-function calculated() {
+function calculated(tell) {
     if (Math.floor(Math.random() * 2)) {
         console.log('win');
-h2.innerHTML= 'you win'
+        h2.innerHTML = 'you win'
+        // return 'win'+ hed 
     } else {
         console.log('lose');
-        h2.innerHTML= 'you lose'
+        h2.innerHTML = 'you lose'
+        // return 'lose'+ tell 
 
+    }
+    // console.log(tell);
+    if (tell === 'tell') {
+        console.log('you choose tell');
+        h22.innerHTML = 'you Choose Tell'
+
+    } else {
+        console.log('you choose head');
+        h22.innerHTML = 'you choose Head'
     }
 }
